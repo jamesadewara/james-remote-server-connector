abc5c09e69e79f5931da92d3d828e3d98156d572
unknown <jamesadewara1@gmail.com>
2026-01-20 01:07:19 +0100

    fix: build error due to node-ssh by adding nodessh as serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['ssh2', 'node-ssh'],
   images: {
     unoptimized: true,
   }
